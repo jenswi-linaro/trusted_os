aa11a99c6da77abd89d463b0df265bad5fc7fbe8
Jens Wiklander <jens.wiklander@linaro.org>
2014-01-27 11:46:06 +0100

    plat-vexpress-a15: default debug enable

--- arch/arm32/plat-vexpress-a15/conf.mk
+++ arch/arm32/plat-vexpress-a15/conf.mk
@@ -4,9 +4,15 @@ CROSS_COMPILE	?= $(CROSS_PREFIX)-
 ARCH_DIR		= arch/$(ARCH)
 
 PLATFORM_CPUARCH	= cortex-a15
-PLATFORM_CFLAGS	 	= -Os -mcpu=$(PLATFORM_CPUARCH) -mthumb
+PLATFORM_CFLAGS	 	= -mcpu=$(PLATFORM_CPUARCH) -mthumb
 PLATFORM_SFLAGS	 	= -mcpu=$(PLATFORM_CPUARCH)
 PLATFORM_CPPFLAGS	= -I$(ARCH_DIR)/include -DNUM_CPUS=1 -DNUM_THREADS=2
 
+DEBUG		?= 1
+ifeq ($(DEBUG),1)
+PLATFORM_CFLAGS += -O0 -g
+else
+PLATFORM_CFLAGS += -Os
+endif
 
 SUBDIRS += $(addprefix $(ARCH_DIR)/, kern libc sm)
