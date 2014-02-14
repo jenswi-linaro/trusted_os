550ca2bf1f35ba7338c2f41ee7eb7eb46826a643
Jens Wiklander <jens.wiklander@linaro.org>
2014-02-14 08:07:34 +0100

    Add STATIC_ASSERT

--- include/kern/kern.h
+++ include/kern/kern.h
@@ -35,4 +35,14 @@
 /* Round down the even multiple of size, size has to be a multiple of 2 */
 #define ROUNDDOWN(v, size) ((v) & ~(size - 1))
 
+/*
+ * Note we need the 2 concats below because arguments to ##
+ * are not expanded, so we need to expand __LINE__ with one indirection
+ * before doing the actual concatenation.
+ */
+#define ASSERT_CONCAT_(a, b) a##b
+#define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
+#define STATIC_ASSERT(e) \
+		enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
+
 #endif /*KERN_KERN_H*/
